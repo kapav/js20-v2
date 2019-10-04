0201e5188502bcf8fe0e8b8a52c847813b9e30fb
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-10-04 16:42:37 +0400

    Added attributive and structural directives as well as two-way binding in template-driven forms.

--- src/app/shared/components/slope/slope.component.ts
+++ src/app/shared/components/slope/slope.component.ts
@@ -7,8 +7,47 @@ import {Component, OnInit} from '@angular/core';
 })
 export class SlopeComponent implements OnInit {
 
+  xValue: number = 0
+  yValue: number = 0
+  zValue: number = 0
+  result: number
+
+  settings = {
+    accent: true,
+    error: true,
+    highlighted: false
+  }
+
+  showUl: boolean = true
+  items: string[] = [
+    'Первый', 'Второй', 'Третий', 'Четвёртый', 'Пятый'
+  ]
+
   constructor() {}
 
   ngOnInit() {}
+
+  calculate() {
+    this.result = +this.xValue + +this.yValue + +this.zValue;
+  }
+
+  reset() {
+    this.xValue = 0;
+    this.yValue = 0;
+    this.zValue = 0;
+    this.result = undefined;
+  }
+
+  getStyles() {
+    return {
+      "background-color": "yellow",
+      "font-weight": "bold",
+      "font-size": "20px"
+    }
+  }
+
+  toggleVisibility() {
+    this.showUl = !this.showUl;
+  }
 
 }
